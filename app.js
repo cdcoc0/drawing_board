e3dbf63a612e0850815dbd2bf2bd1ca9d8e477fd
velykitty <jiyunee13@gmail.com>
2020-11-02 11:56:06 +0900

    context -control canvas -draw lines on the canvas

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+//https://developer.mozilla.org/ko/docs/Web/API/CanvasRenderingContext2D
+
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 600;
+canvas.height = 600;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,22 +15,25 @@ function stopPainting() {
     painting = false;
 }
 
-function onMouseMove(event){
-    const x = event.offsetX;
-    const y = event.offsetY;
-}
-
-function onMouseDown(event) {
+function startPainting() {
     painting = true;
 }
 
-function onMouseUp(event) {
-    stopPainting();
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else { //canvas 픽셀 사이즈 필(pixel modifier)
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown); //click
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting); //click
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting); //mouse goes ouside the canvas
 }
